e3ba2b6ebe3edd1ee8ba205c4deabf5c713af9fc
Abdurrahman Hafez <abdurrahman.hafez@gmail.com>
2020-10-09 15:22:46 +0200

    Add semantic ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'semantic-ui-react';
+
 
 function App() {
   return (
     <div className="App">
+      <Button color="red">Hello</Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
